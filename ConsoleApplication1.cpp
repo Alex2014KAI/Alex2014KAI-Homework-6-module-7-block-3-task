dba6d45c224d206cb7bdf5f06581f0b6f8442884
Alexei Petrov <petrovAlekse95@mail.ru>
2025-05-18 21:15:07 +0300

    Initial commit

---
+++ ConsoleApplication1.cpp
@@ -0,0 +1,54 @@
+﻿#include <iostream>
+
+template<class TType> class UP {
+public:
+    UP(TType* aData) :mData{ aData } {};
+    UP(const UP&) = delete;//
+    //
+    ~UP() {
+        if (mData) {
+            delete mData;
+        }
+    };//
+    //
+    void reset(TType* aData) {
+        if (mData = aData) return;
+        if (mData) delete mData;
+        mData = aData;
+    };//
+    //
+    TType* release() {
+        TType* oldData = mData;
+        mData = nullptr;
+        return oldData;
+    };//
+    //
+    
+    TType& operator*() const {
+        return *mData;
+    };//
+    //
+
+private:
+    UP &operator = (const UP& aOther) = delete;
+    TType* mData{ nullptr };
+};
+
+
+
+
+struct Node {
+    Node() { std::cout << "Node\n"; };
+    ~Node() { std::cout << "~Node\n"; };
+    void data() { std::cout << "HELLO\n"; };
+    int dataNode{ 10 };
+};
+
+int main()
+{
+    UP<Node> ptrNode(new Node());
+    (*ptrNode).data();
+    std::cout << (*ptrNode).dataNode << std::endl;
+
+
+}
